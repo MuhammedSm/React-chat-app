a5a431356e17c2da0c36a8d7e7d0dee5315ec45e
MuhammedSm <muhammedseyhmusa5@gmail.com>
2023-06-17 00:10:17 +0300

    Login fix

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,41 +1,41 @@
-import React, { useState } from 'react'
-import Add from "../img/addAvatar.png" 
-import {createUserWithEmailAndPassword, updateProfile } from "firebase/auth";
-import {auth, storage,db} from '../firebase'
-import {ref, uploadBytesResumable, getDownloadURL } from "firebase/storage"; 
-import { doc, setDoc } from "firebase/firestore";
-import { useNavigate } from 'react-router-dom';
-const Login=()=> {
-  const [err,setErr]=useState(false);
-  const navigate=useNavigate()
-  const handleSubmit =async(e)=>{
-      e.preventDefault();
-      const email=e.target[0].value;
-      const password = e.target[1].value;
-     
+import React, { useState } from "react";
+import { useNavigate, Link } from "react-router-dom";
+import { signInWithEmailAndPassword } from "firebase/auth";
+import { auth } from "../firebase";
 
-      try{
-      }catch(err){
-        setErr(true)
-      }
+const Login = () => {
+  const [err, setErr] = useState(false);
+  const navigate = useNavigate();
 
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    const email = e.target[0].value;
+    const password = e.target[1].value;
+
+    try {
+      await signInWithEmailAndPassword(auth, email, password);
+      navigate("/")
+    } catch (err) {
+      setErr(true);
+    }
   };
   return (
     <div className="formContainer">
-    <div className="formWrapper">
-        <span className="logo">Muhammed Chat</span>
-        <span className="title">Register</span>
-       <form onSubmit={handleSubmit}>
-            <input type="email" placeholder="email" />
-            <input type="password" placeholder="password" />
-            <input style={{display:"none"}} type="file" id="file"/>
-            
-            <button>Sign in</button>
-       </form>
-       <p>Don't have an account? Register</p>
+      <div className="formWrapper">
+        <span className="logo">Lama Chat</span>
+        <span className="title">Login</span>
+        <form onSubmit={handleSubmit}>
+          <input type="email" placeholder="email" />
+          <input type="password" placeholder="password" />
+          <button>Sign in</button>
+          {err && <span>Something went wrong</span>}
+        </form>
+        <p>You don't have an account? <Link to="/register">Register</Link></p>
+      </div>
     </div>
-    
-  </div>
   );
 };
+
 export default Login;
+
+    
