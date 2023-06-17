a89c00502c048a14043f19afee65197c0ad4e7c2
MuhammedSm <muhammedseyhmusa5@gmail.com>
2023-06-17 20:49:08 +0300

    popUp notification/style updates

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import Navbar from './Navbar';
 import Search from './Search';
 import Chats from './Chats';
@@ -10,6 +10,31 @@ const Sidebar = () => {
       sidebar.style.display = 'none';
     }
   };
+
+  useEffect(() => {
+    const showNotification = () => {
+      if (window.innerWidth <= 480) {
+        const notification = document.createElement('div');
+        notification.classList.add('popup-notification');
+        notification.innerText = 'Not compatible with small devices please switch to Desktop or Tablet';
+
+        document.body.appendChild(notification);
+
+        setTimeout(() => {
+          document.body.removeChild(notification);
+        }, 3000);
+      }
+    };
+
+    showNotification();
+
+    // Add resize event listener to show/hide notification dynamically
+    window.addEventListener('resize', showNotification);
+
+    return () => {
+      window.removeEventListener('resize', showNotification);
+    };
+  }, []);
 
   return (
     <div className='sidebar'>
