76d0887f69ce1e33dae6cad546bb3c87013a25b2
MuhammedSm <muhammedseyhmusa5@gmail.com>
2023-06-17 15:11:56 +0300

    some changes

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,15 +1,23 @@
-import React from 'react'
-import Navbar from "./Navbar"
-import Search from './Search'
-import Chats from './Chats'
+import React from 'react';
+import Navbar from './Navbar';
+import Search from './Search';
+import Chats from './Chats';
+
 const Sidebar = () => {
+  const handleUserChatClick = () => {
+    if (window.innerWidth <= 480) {
+      const sidebar = document.querySelector('.sidebar');
+      sidebar.style.display = 'none';
+    }
+  };
+
   return (
     <div className='sidebar'>
-      <Navbar/>
-      <Search/>
-      <Chats/>
+      <Navbar />
+      <Search />
+      <Chats onUserChatClick={handleUserChatClick} />
     </div>
-  )
-}
+  );
+};
 
-export default Sidebar
+export default Sidebar;
